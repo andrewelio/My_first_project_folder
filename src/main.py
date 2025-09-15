448c35c81f2a471d4acb786999edbe8faebb1b63
Andrew Elio <andrewelio95@gmail.com>
2025-09-14 20:04:19 -0500

    Add hello message to main.py

--- src/main.py
+++ src/main.py
@@ -2,3 +2,4 @@ import requests
 r = requests.get("https://api.github.com")
 print("Status:", r.status_code)
 print("Keys:", list(r.json().keys())[:5])
+print("Hello, Github! My first tracked change.")
